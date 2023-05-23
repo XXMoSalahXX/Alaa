5257d785188b144226a5f8df22ec8d3411ad531a
XXMoSalahXX <96550621+XXMoSalahXX@users.noreply.github.com>
2023-05-23 21:28:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+    "use strict";
+
+    var typed = new Typed('.type', {
+        strings: [`Happy birthday 🎂, yarab tkon sana agml bkter mn kol ally fat, btmnalk kol altwfe2 fe haytk wtkony as3d bkter fe ally gay, olt a3mlk haga llzkra wtfdl forever yarb t3gbk, mt25ra bs 7lwa :)`],
+        typeSpeed: 80
+    });
+})
